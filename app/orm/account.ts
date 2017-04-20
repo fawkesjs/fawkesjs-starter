8f0549d6f19e861f6ac1480b8c016d73bc973f6c
Ng Heng Lim <nghenglim@gmail.com>
2017-04-20 09:40:07 +0800

    update new rule for tslint

--- app/orm/account.ts
+++ app/orm/account.ts
@@ -3,44 +3,44 @@ import * as Sequelize from "sequelize";
 export class AccountOrm {
   public static definition(sequel: Sequelize.Instance) {
     const Account = sequel.define("Account", {
-      "createdAt": {
-        "allowNull": false,
-        "field": "created_at",
-        "type": Sequelize.DATE,
+      createdAt: {
+        allowNull: false,
+        field: "created_at",
+        type: Sequelize.DATE,
       },
-      "email": {
-        "allowNull": false,
-        "type": Sequelize.STRING(128),
-        "unique": true,
-        "validate": {
-          "isEmail": true,
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(128),
+        unique: true,
+        validate: {
+          isEmail: true,
         },
       },
-      "id": {
-        "allowNull": false,
-        "defaultValue": Sequelize.UUIDV4,
-        "primaryKey": true,
-        "type": Sequelize.UUID,
-        "validate": {
-          "isUUID": 4,
+      id: {
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        validate: {
+          isUUID: 4,
         },
       },
-      "name": {
-        "allowNull": false,
-        "type": Sequelize.STRING(128),
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(128),
       },
-      "password": {
-        "allowNull": false,
-        "type": Sequelize.STRING(128),
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(128),
       },
-      "updatedAt": {
-        "field": "updated_at",
-        "type": Sequelize.DATE,
+      updatedAt: {
+        field: "updated_at",
+        type: Sequelize.DATE,
       },
     },
       {
-        "tableName": "account",
-        "timestamps": true,
+        tableName: "account",
+        timestamps: true,
       });
     return Account;
   }
